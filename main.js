a40708eebfd4c8fde41cdc71ba12a4537683c16d
walleser-karli <k.walleser@ufl.edu>
2021-07-14 20:11:35 -0400

    Added IDs and classes to make JavaScript toggle work so submenus can appear

--- main.js
+++ main.js
@@ -1,1 +1,47 @@
-// It is always helpful to use comments in your code!
+var showDropdown = document.getElementById("products");
+
+showDropdown.addEventListener("mouseenter",
+  function() {
+   document.getElementById("product-list").classList.remove("hide1");
+   document.getElementById("product-list").classList.add("show");
+  }, false
+);
+
+showDropdown.addEventListener("mouseleave",
+  function() {
+   document.getElementById("product-list").classList.remove("show");
+   document.getElementById("product-list").classList.add("hide1");
+  }, false
+);
+
+var showDropdown2 = document.getElementById("services");
+
+showDropdown2.addEventListener("mouseenter",
+  function() {
+   document.getElementById("services-list").classList.remove("hide2");
+   document.getElementById("services-list").classList.add("show");
+  }, false 
+);
+
+showDropdown2.addEventListener("mouseleave",
+  function() {
+   document.getElementById("services-list").classList.remove("show");
+   document.getElementById("services-list").classList.add("hide2");
+  }, false 
+);
+
+var showDropdown3 = document.getElementById("company");
+
+showDropdown3.addEventListener("mouseenter",
+  function() {
+   document.getElementById("company-list").classList.remove("hide3");
+   document.getElementById("company-list").classList.add("show");
+  }, false
+);
+
+showDropdown3.addEventListener("mouseleave",
+  function() {
+   document.getElementById("company-list").classList.remove("show");
+   document.getElementById("company-list").classList.add("hide3");
+  }, false
+);
